1372bb81459c7e914b70d971648a74b2cf26d701
zjb6376 <956673039@qq.com>
2020-08-11 21:20:36 +0800

    081101

---
+++ src/main/java/com/xiaoming/test/pojo/Student.java
@@ -0,0 +1,29 @@
+package com.xiaoming.test.pojo;
+
+/**
+ * @Author:zjb
+ * @Date:2020/8/11
+ * @Version 1.0
+ */
+public class Student {
+
+    private String id;
+
+    private String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
