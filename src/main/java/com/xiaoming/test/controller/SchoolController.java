c87b2bdfd24ea82c4511d2316a48ee44d9c99d61
zjb6376 <956673039@qq.com>
2020-08-11 21:55:50 +0800

    081103

---
+++ src/main/java/com/xiaoming/test/controller/SchoolController.java
@@ -0,0 +1,11 @@
+package com.xiaoming.test.controller;
+
+/**
+ * @Author:zjb
+ * @Date:2020/8/11
+ * @Version 1.0
+ */
+public class SchoolController {
+
+    private static final String SCHOOL_NAME = "南湖二小";
+}
